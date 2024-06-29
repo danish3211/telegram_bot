0ea211c02698b26ce4f6296464c3fc9eec6e5837
danish sheikh <130551259+danish3211@users.noreply.github.com>
2024-06-29 20:13:10 +0530

    added bot code

--- src/App.js
+++ src/App.js
@@ -1,69 +1,3 @@
-// import React, { useState } from "react";
-// import axios from "axios";
-// function App() {
-
-//   const [data , setData] = useState({})
-//   const [location , setLocation] = useState('')
-//   const url =
-//     `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=f66556f693d2a555512b76af4ed62cce`;
-
-// const searchLocation =(event) => {
-//   if (event.key ==='Enter'){
-//     axios.get(url).then((response) =>{
-//       setData(response.data)
-//       console.log(response.data)
-//     })
-//     setLocation('')
-//   }
-
-// }
-
-//   return (
-//     <div className="App">
-//       <div className="search">
-//         <input
-//         value={location}
-//         onKeyPress={searchLocation}
-//         onChange={event =>setLocation(event.target.value)}
-//         placeholder="Enter Location"
-//         type="text"/>
-//       </div>
-//       <div className="container">
-//         <div className="top">
-//           <div className="location">
-//             <p>{data.name}</p>
-//           </div>
-//           <div className="temp">
-//             {data.main ? <h1>{data.main.temp.toFixed()}°F</h1>:null}
-//           </div>
-//           <div className="desc">
-//             {data.weather ? <p>{data.weather[0].main}</p> :null}
-//           </div>
-//         </div>
-
-//         {data.name !=undefined &&
-//         <div className="bottom">
-//         <div className="feels">
-//           {data.main ?<p className="bold">{data.main.feels_like}</p>:null}
-//           <p>Feels like</p>
-//         </div>
-//         <div className="humidity">
-//           {data.main ?<p className="bold">{data.main.humidity.toFixed()}%</p>:null}
-//           <p>Humidity</p>
-//         </div>
-//         <div className="wind">
-//           {data.wind ?<p className="bold">{data.wind.speed.toFixed()} km/h</p>:null}
-//          <p>Wind speed</p>
-//         </div>
-//       </div>
-//         }
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from "react";
 import axios from "axios";
 import video from "./assets/video.mp4";
@@ -71,6 +5,7 @@ import sunnyVideo from "./assets/sunny.mp4";
 import hazeVideo from "./assets/haze.mp4";
 import cloudyVideo from "./assets/cloudy.mp4";
 import rainyVideo from "./assets/rainy.mp4";
+import snowVideo from "./assets/snow.mp4";
 
 function App() {
   const [data, setData] = useState({});
@@ -111,6 +46,8 @@ function App() {
         return rainyVideo;
       case "Haze":
         return hazeVideo;
+        case "Snow":
+        return snowVideo;
       default:
         return video; // Default to sunny video
     }
